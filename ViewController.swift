e56753d52d0503ba9c03374a7ba9ab4446845f4f
cannon <romanghiet05@gmail.com>
2021-04-24 09:16:17 -0700

    UI initials to controller

--- ViewController.swift
+++ ViewController.swift
@@ -2,6 +2,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //UI initials
+        @IBOutlet weak var leftImg: UIImageView!
+        @IBOutlet weak var rightImg: UIImageView!
+        @IBOutlet weak var playerScore: UILabel!
+        @IBOutlet weak var cpuScore: UILabel!
+        
+        //variabls Initials
+        var playerCounter = 0
+        var cpuCounter  = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
